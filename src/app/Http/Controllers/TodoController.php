0abfeea1092159a3104e00e7fab19f58369ae442
yutaka-fujise <yutaka.f-5112@docomo.ne.jp>
2025-10-14 09:59:35 +0900

    追加機能実装・コントローラー修正まで

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
      public function index()
    {
-     return view('index');
+    $todos = Todo::all();
+    //  return view('index');
+    return view('index', compact('todos'));
    }
 //    index.blade.phpを呼び出す処理
+   public function store(Request $request)
+     {
+         $todo = $request->only(['content']);
+         Todo::create($todo);
+
+         return redirect('/');
+     }
 }
